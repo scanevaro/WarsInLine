411b324655f51df7059f92853ed495049c168f41
Sebastián <scanevaro@hotmail.com>
2014-12-13 17:56:18 -0300

    Initial commit

---
+++ android/src/com/deeep/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.deeep.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.deeep.game.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
